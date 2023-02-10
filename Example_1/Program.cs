33f968c78597947bb40efaa0b9e405b1e49d5987
Lagutina Elizaveta <below8585@mail.ru>
2023-02-10 15:05:23 +0300

    Чужой квадрат числа

---
+++ Example_1/Program.cs
@@ -0,0 +1,5 @@
+﻿// Чужой квадрат числа
+Console.WriteLine("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+int resalt = numberA*numberA;
+Console.WriteLine("Квадрат числа равен: " + resalt);
